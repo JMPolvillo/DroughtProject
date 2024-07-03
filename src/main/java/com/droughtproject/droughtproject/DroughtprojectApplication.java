86f33ce328e12b8e1ba976bb9d4aa57795b65b8b
JMPolvillo <rothkildam@gmail.com>
2024-07-03 20:16:58 +0200

    first commit

---
+++ src/main/java/com/droughtproject/droughtproject/DroughtprojectApplication.java
@@ -0,0 +1,13 @@
+package com.droughtproject.droughtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroughtprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroughtprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/droughtproject/droughtproject/DroughtprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droughtproject.droughtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroughtprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
